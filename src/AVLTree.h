633a1476e5bfd913df9e881c6e57a6b05bfe01a3
wzhe <ahuwang@163.com>
2018-12-28 19:51:36 +0800

    add AVL Tree

---
+++ src/AVLTree.h
@@ -0,0 +1,31 @@
+/***************************************************
+ * Data Structure & Algorithm
+ * wzhe ahuwang@163.com
+ ***************************************************/
+
+#pragma once
+
+#include "BinSearchTree.h"
+
+template<typename T> class AVL : public BST<T> {
+public:
+    BinNodePosi(T) insert override(const T& e);
+    bool remove override(const T& e);
+    //BST::search()等其余接口可直接沿用
+}；
+
+#define Balanced(x) (stature((x).lChild) == stature((x).rChild)) //理想平衡条件
+#define BalFac(x) (stature((x).lChild) - stature((x).rChild)) //平衡因子
+#define AvlBalanced(x) ((-2 < BalFac(x)) && (BalFac(x) < 2)) //AVL平衡条件
+/***************************************************
+ * 在左、右孩子中取更高者，若等高，则与父亲p同侧者优先
+ * 在AVL平衡调整前，借此确定重构方案
+ ***************************************************/
+#define tallerChild(x) ( \
+    stature((x)->lChild) > stature((x)->rChild) ? (x)->lChild : ( \
+    stature((x)->rChild) > stature((x)->lChild) ? (x)->rChild : ( \
+    IsLChild(*(x)) ? (x)->lChild : (x)->rChild \
+    ) \
+    ) \
+)
+#include "AVL_implementation.h"
--- src/BinSearchTree_implementation.h
+++ src/BinSearchTree_implementation.h
@@ -51,18 +51,6 @@ bool BST<T>::remove(const T& e)
 }
 
 template<typename T>
-BinNodePosi(T) BST<T>::connect34(
-    BinNodePosi(T), BinNodePosi(T), BinNodePosi(T),
-    BinNodePosi(T), BinNodePosi(T), BinNodePosi(T), BinNodePosi(T) )
-{
-}
-
-template<typename T>
-BinNodePosi(T) BST<T>::rotateAt( BinNodePosi(T) x)
-{
-}
-
-template<typename T>
 BinNodePosi(T) BST<T>::insert(const T& e)
 {
     BinNodePosi(T) & x = search(e);
@@ -72,3 +60,48 @@ BinNodePosi(T) BST<T>::insert(const T& e)
     this->updateHeightAbove(x);
     return x;
 }
+
+/******************************************************************************************
+* 按照“3 + 4”结极联接3个节点及其四棵子树，迒回重组乀后癿尿部子树根节点位置（即b）
+* 子树根节点不上局节点乀间癿双向联接，均项由上局调用者完成
+* 可用亍AVL和RedBlack癿尿部平衡调整
+******************************************************************************************/
+template<typename T> BinNodePosi(T) BST<T>::connect34(
+    BinNodePosi(T) a, BinNodePosi(T) b, BinNodePosi(T) c,
+    BinNodePosi(T) T0, BinNodePosi(T) T1, BinNodePosi(T) T2, BinNodePosi(T) T3
+) {
+    a->lChild = T0; if (T0) T0->parent = a;
+    a->rChild = T1; if (T1) T1->parent = a; updateHeight(a);
+
+    c->lChild = T2; if (T2) T2->parent = c;
+    c->rChild = T3; if (T3) T3->parent = c; updateHeight(c);
+
+    b->lChild = a; a->parent = b;
+    b->rChild = c; c->parent = b; updateHeight(b);
+    return b;
+}
+
+/******************************************************************************************
+* BST节点旋转发换统一算法（3节点 + 4子树），迒回调整乀后尿部子树根节点癿位置
+* 注意：尽管子树根会正确指向上局节点（如枅存在），但反向癿联接项由上局函数完成
+******************************************************************************************/
+template <typename T> BinNodePosi(T) BST<T>::rotateAt(BinNodePosi(T) v) { //v为非空癿孙辈节点
+	BinNodePosi(T) p = v->parent; BinNodePosi(T) g = p->parent; //规v、p和g相对位置分四种情冴
+	if (IsLChild(*p)) /* zig */
+		if (IsLChild(*v)) { /* zig-zig */
+			p->parent = g->parent; //向上联接
+			return connect34(v, p, g, v->lChild, v->rChild, p->rChild, g->rChild);
+		} else { /* zig-zag */
+			v->parent = g->parent; //向上联接
+			return connect34(p, v, g, p->lChild, v->lChild, v->rChild, g->rChild);
+		}
+		else /* zag */
+			if (IsRChild(*v)) { /* zag-zag */
+				p->parent = g->parent; //向上联接
+				return connect34(g, p, v, g->lChild, p->lChild, v->lChild, v->rChild);
+			} else { /* zag-zig */
+				v->parent = g->parent; //向上联接
+				return connect34(g, v, p, g->lChild, v->lChild, v->rChild, p->rChild);
+			}
+}
+
